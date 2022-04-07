95801ca7737d2ba8bb1d8e30f3a3d722e45dd44d
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-04-07 20:32:04 +0900

    #1.8 nicknames

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,45 @@
-alert("hi");
+
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
+//frontend의 socket : 서버로의 연결
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+//JSON object -> String
+function makeMessage(type, payload){
+    const msg = {type, payload}
+    return JSON.stringify(msg);
+}
+
+//open
+socket.addEventListener("open", ()=>{
+    console.log("Connected to Server ✅");
+})
+
+//message
+socket.addEventListener("message", (message)=>{
+    //message를 ul안에 넣어주기!
+   const li = document.createElement("li");
+   li.innerText = message.data;
+   messageList.append(li);
+});
+
+//close
+socket.addEventListener("close", ()=>{
+    console.log("Disconnected to Server")
+})
+
+//send a message to backend
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value="";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nikcname", input.value));
+}
+
--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from "express";
-//import path from 'path';
 
-
-//const __dirname = path.resolve();
 const app = express();
 
 //pug 페이지들을 렌더하기 위해 pug설정
@@ -10,14 +9,52 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-//route 생성
-//express로 views를 설정해주고 render해준다.
+//route 생성 : express로 views를 설정해주고 render해준다.
 //catchall url : 사용자가 어떤 url로 이동하던지 홈으로 돌려보낸다.
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
-//app.get("/", (req, res) => res.render("home"));
 
-console.log("hello");
+//http server 설정
+const handleListen = () => console.log('Listening on ws && http://localhost:3000');
 
-const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+//server에서 websocket을 만들 수 있도록 준비
+//같은 서버에서 http, websocket 둘 다 작동하도록 http server 위에 websocket 서버 생성 
+const server = http.createServer(app);
+
+//websocket server 생성  
+//http server를 사용하고 싶지 않을 때는 websocket server(아래)만 만들면 된다.
+                                    // 여기서 서버를 전달해준다. 
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose(){
+    console.log("Disconnected from the Browser");
+}
+
+//fake database 누군가 서버에 연결하면 해당 connection을 담아준다.
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous"; //익명으로 챗에 참여한 사람들의 닉네임을 설정
+    console.log("✅ Connected to Browser");
+    socket.on("close", onSocketClose);
+    socket.on("message", (msg) => {
+
+        //JSON.stringify : convert javascript object to String
+        const message = JSON.parse(msg);
+
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(message.payload.toString('utf8')));
+
+            case "nickname":
+                //type : 메시지 종류, payload : 메시지에 담겨있는 중요한 정보들
+                socket["nickname"] = message.payload;
+        }
+    
+        
+    });
+});
+
+server.listen(3000, handleListen);
+
