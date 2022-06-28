d3383957ad1093e97acf354df6ccd6de30eb0ea6
ArchivvonJang <76926509+ArchivvonJang@users.noreply.github.com>
2022-06-29 00:13:26 +0900

    ♻️ Fix error & Refactor code

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,13 +8,13 @@ const socket = new WebSocket(`ws://${window.location.host}`);
 //메시지를 만들고 stringify 해주는 함수
 //JSON object -> String
 function makeMessage(type, payload){
-    const msg = {type, payload}
-    return JSON.stringify(msg);
+  const msg = {type, payload}
+  return JSON.stringify(msg);
 }
 
 
 function handleOpen(){
-    console.log("Connected to Server✅");
+  console.log("[Front] ✅ Connected to Server");
 }
 
 //open
@@ -23,34 +23,42 @@ socket.addEventListener("open", handleOpen);
 
 //message
 socket.addEventListener("message", (message)=>{
-    //message를 ul안에 넣어주기!
-   const li = document.createElement("li");
-   li.innerText = message.data;
-   messageList.append(li);
+  //message를 ul안에 넣어주기!
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 //close
 socket.addEventListener("close", ()=>{
-    console.log("Disconnected to Server")
+  console.log("[Front] Disconnected to Server")
 })
 
 //send a message to backend
 function handleSubmit(event){
-    event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  //                                  type, payload
+  socket.send(makeMessage("new_message", input.value));
 
-    //message를 ul안에 넣어주기!
-    const li = document.createElement("li");
+  //message를 ul안에 넣어주기!
+  /*  const li = document.createElement("li");
     li.innerText = `You: ${input.value}`;
     messageList.append(li);
-    input.value="";
+    input.value="";*/
 }
 
 function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value="";
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  /* socket.send({
+    type: "nickname",
+    payload: input.value
+  });*/
+  console.log(input.value);
+  input.value="";
 }
 
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -23,39 +23,40 @@ const server = http.createServer(app);
 
 //websocket server 생성  
 //http server를 사용하고 싶지 않을 때는 websocket server(아래)만 만들면 된다.
-                                    // 여기서 서버를 전달해준다. 
+// 여기서 서버를 전달해준다.
 const wss = new WebSocket.Server({ server });
 
 function onSocketClose(){
-    console.log("Disconnected from the Browser");
+  console.log("[Back] Disconnected from the Browser");
 }
 
-//fake database 누군가 서버에 연결하면 해당 connection을 담아준다.
+//fake database 누군가 서버에 연결하면 해당 connection 담아준다.
 const sockets = [];
 
 wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anony"; //익명으로 챗에 참여한 사람들의 닉네임을 설정
-    console.log("✅ Connected to Browser");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
+  //브라우저에 연결될 때, 브라우저를 sockets array 에 담아준다.
+  sockets.push(socket);
 
-        //JSON.stringify : convert javascript object to String
-        const message = JSON.parse(msg);
+  socket["nickname"] = "Anony"; //익명으로 챗에 참여한 사람들의 닉네임을 설정
+  console.log("[Back] ✅ Connected to Browser");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
 
-        switch(message.type){
-            case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-                break;
-            case "nickname":
-                //type : 메시지 종류, payload : 메시지에 담겨있는 중요한 정보들
-                //socket[여기에 정보를 저장할 수 있다. ]
-                socket["nickname"] = message.payload;
-                break;
-        }
-    
-        
-    });
+    //JSON.stringify : convert javascript object to String
+    const message = JSON.parse(msg);
+
+    switch(message.type){
+      case "new_message":
+        //각 브라우저를 aSocket 으로 표시하고 메시지를 보낸다.
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case "nickname":
+        //type : 메시지 종류, payload : 메시지에 담겨있는 중요한 정보들
+        //socket[여기에 정보를 저장할 수 있다. ]
+        socket["nickname"] = message.payload;
+        break;
+    }
+  });
 });
 
 server.listen(3000, handleListen);
